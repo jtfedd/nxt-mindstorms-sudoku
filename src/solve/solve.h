e64091eef2d9f26a03f608c2e6090eeb3a26c960
Jacob Feddersen <jtfedd@gmail.com>
2021-07-28 22:13:23 -0500

    get backtracking finally working

--- src/solve/solve.h
+++ src/solve/solve.h
@@ -4,34 +4,35 @@
 #include "src/solve/utils.h"
 #include "src/solve/solve_context.h"
 
-bool SolveSudoku(int &board[][], int &iterations, int &guesses) {
+bool SolveSudoku(int &src_board[][], int &iterations, int &guesses) {
   iterations = 0;
   guesses = 0;
-
-  int guess_depth = 0;
 
   bool foundNewSolution = true;
   bool invalid = false;
 
-  solve_context ctx;
-  init_solve_context(ctx, board);
+  init_solve_context(src_board);
 
-  while (foundNewSolution && ctx.solved_count < 81) {
+  while (foundNewSolution && solved_count < 81) {
     iterations++;
     foundNewSolution = false;
     invalid = false;
+
     for (int r = 0; r < 9 && !invalid; r++) {
       for (int c = 0; c < 9 && !invalid; c++) {
-        if (ctx.board[r][c] != 0) {
+        // Skip if there's already a value here
+        if (solve_board[r][c] != 0) {
           continue;
         }
 
-        if (singleBitSet(ctx.position_possibilities[r][c])) {
-            int value = bitToNum(ctx.position_possibilities[r][c]);
-            set_value(ctx, r, c, value, guess_depth, false);
-            foundNewSolution = true;
-        } else if (ctx.position_possibilities[r][c] == 0) {
-          // If no number can go here, well...
+        int possibility = getSinglePossibility(r, c);
+
+        if (possibility > 0) {
+          // If there's only one number that can go here, then place it
+          set_value(r, c, possibility, false);
+          foundNewSolution = true;
+        } else if (possibility == -1) {
+          // If there are no possibilities at this position, then this board is invalid
           invalid = true;
         }
       }
@@ -46,13 +47,13 @@ bool SolveSudoku(int &board[][], int &iterations, int &guesses) {
 
       for (int r = 0; r < 9; r++) {
         for (int c = 0; c < 9; c++) {
-          if (ctx.board[r][c] != 0) {
+          if (solve_board[r][c] != 0) {
             continue;
           }
 
-          int numBitsSet = countBits(ctx.position_possibilities[r][c]);
-          if (numBitsSet < minPossibilities) {
-            minPossibilities = numBitsSet;
+          int numPossibilities = countPossibilities(r, c);
+          if (numPossibilities < minPossibilities) {
+            minPossibilities = numPossibilities;
             minR = r;
             minC = c;
           }
@@ -64,44 +65,41 @@ bool SolveSudoku(int &board[][], int &iterations, int &guesses) {
         invalid = true;
       } else {
         foundNewSolution = true;
-        return false;
 
-        int numToGuess = nextPossibility(ctx.position_possibilities[minR][minC], 0);
+        int numToGuess = nextPossibility(minR, minC, 0);
         guess_depth++;
-        set_value(ctx, minR, minC, numToGuess, guess_depth, true);
+        set_value(minR, minC, numToGuess, true);
 
         guesses++;
       }
     }
 
-    if (invalid && guess_depth > 0) {
-      while (invalid && guess_depth > 0) {
-        // Restore to the last guess and guess the next number.
-        // If there's no guess to restore to, then we are out of luck and the board is unsolveable.
-        int guess_row = 0;
-        int guess_col = 0;
-        int guess = 0;
+    while (invalid && guess_depth > 0) {
+      // Restore to the last guess and guess the next number.
+      // If there's no guess to restore to, then we are out of luck and the board is unsolveable.
+      int guess_row = 0;
+      int guess_col = 0;
+      int guess = 0;
 
-        reset_guess(ctx, guess_depth, guess_row, guess_col, guess);
-        guess_depth--;
+      reset_guess(guess_row, guess_col, guess);
+      guess_depth--;
 
-        int next_guess = nextPossibility(ctx.position_possibilities[guess_row][guess_col], guess);
-        
-        if (next_guess > 0) {
-          guess_depth++;
-          set_value(ctx, guess_row, guess_col, next_guess, guess_depth, true);
+      int next_guess = nextPossibility(guess_row, guess_col, guess);
+      
+      if (next_guess > 0) {
+        guess_depth++;
+        set_value(guess_row, guess_col, next_guess, true);
 
-          guesses++;
-          foundNewSolution = true;
-          invalid = false;
-        }
+        guesses++;
+        foundNewSolution = true;
+        invalid = false;
       }
     }
   }
 
-  copy_board(board, ctx.board);
+  copy_board(src_board, solve_board);
 
-  return is_valid_solution(ctx);
+  return is_valid_solution();
 }
 
 #endif
--- src/solve/solve_context.h
+++ src/solve/solve_context.h
@@ -1,128 +1,135 @@
 #ifndef SOLVE_CONTEXT_H
 #define SOLVE_CONTEXT_H
 
+#define ALL_POSSIBILITIES 0x3FE
+
 #include "src/solve/utils.h"
-#include "src/bluetooth/bt_print.h"
 
-struct solve_context {
-    int solved_count;
-    int board[][];
-    int position_possibilities[][];
-    int guess_depths[][];
-    bool guesses[][];
-};
+int solved_count;
+int guess_depth;
+int solve_board[9][9];
+int guess_depths[9][9];
+bool guesses[9][9];
 
-int nextPossibility(int x, int last_guess) {
+int rows[9];
+int cols[9];
+int boxes[9];
+
+void set_value(int row, int col, int value, bool is_guess) {
+  solve_board[row][col] = value;
+  guesses[row][col] = is_guess;
+  guess_depths[row][col] = guess_depth;
+  solved_count++;
+
+  int bI = boxIndex(row, col);
+
+  rows[row] |= numToBit(value);
+  cols[col] |= numToBit(value);
+  boxes[bI] |= numToBit(value);
+}
+
+void init_solve_context(int &src_board[][]) {
+  solved_count = 0;
+  guess_depth = 0;
+
+  for (int i = 0; i < 9; i++) {
+    rows[i] = 0;
+    cols[i] = 0;
+    boxes[i] = 0;
+
+    for (int j = 0; j < 9; j++) {
+      solve_board[i][j] = 0;
+      guess_depths[i][j] = 0;
+      guesses[i][j] = false;
+    }
+  }
+
+  for (int i = 0; i < 9; i++) {
+    for (int j = 0; j < 9; j++) {
+      if (src_board[i][j] != 0) {
+        set_value(i, j, src_board[i][j], false);
+      }
+    }
+  }
+}
+
+void reset_guess(int &guess_row, int &guess_col, int &guessed_value) {
+  for (int r = 0; r < 9; r++) {
+    for (int c = 0; c < 9; c++) {
+      if (guess_depths[r][c] == guess_depth) {
+        int value = solve_board[r][c];
+
+        if (guesses[r][c]) {
+          guess_row = r;
+          guess_col = c;
+          guessed_value = value;
+        }
+
+        solved_count--;
+        solve_board[r][c] = 0;
+        guesses[r][c] = false;
+        guess_depths[r][c] = 0;
+
+        int bI = boxIndex(r, c);
+
+        rows[r] &= ~numToBit(value);
+        cols[c] &= ~numToBit(value);
+        boxes[bI] &= ~numToBit(value);
+      }
+    }
+  }
+}
+
+bool isPossibility(int r, int c, int x) {
+  int bI = boxIndex(r, c);
+  int valueBit = numToBit(x);
+
+  bool notInRow = (rows[r] & valueBit) == 0;;
+  bool notInCol = (cols[c] & valueBit) == 0;
+  bool notInBox = (boxes[bI] & valueBit) == 0;
+
+  return notInRow && notInCol && notInBox;
+}
+
+int countPossibilities(int r, int c) {
+  int count = 0;
+  for (int i = 1; i <= 9; i++) {
+    if (isPossibility(r, c, i)) {
+      count++;
+    }
+  }
+  return count;
+}
+
+int getSinglePossibility(int r, int c) {
+
+  int bI = boxIndex(r, c);
+  
+  int singlePossibility = -1;
+
+  for (int i = 1; i <= 9; i++) {
+    if (isPossibility(r, c, i)) {
+      if (singlePossibility > 0) {
+        return 0;
+      }
+      singlePossibility = i;
+    }
+  }
+
+  return singlePossibility;
+}
+
+int nextPossibility(int r, int c, int last_guess) {
   for (int i = last_guess + 1; i < 10; i++) {
-    if (x & (1 << i)) {
+    if (isPossibility(r, c, i)) {
       return i;
     }
   }
   return -1;
 }
 
-void init_board(solve_context &ctx) {
-    int initArray[];
-    ArrayInit(initArray, 0, 9);
-    ArrayInit(ctx.board, initArray, 9);
-}
-
-void init_possibilities(solve_context &ctx) {
-    int initArray[];
-    ArrayInit(initArray, 0x3FE, 9);
-    ArrayInit(ctx.position_possibilities, initArray, 9);
-}
-
-void init_depths(solve_context &ctx) {
-    int initArray[];
-    ArrayInit(initArray, 0, 9);
-    ArrayInit(ctx.guess_depths, initArray, 9);
-}
-
-void init_guesses(solve_context &ctx) {
-    bool initArray[];
-    ArrayInit(initArray, false, 9);
-    ArrayInit(ctx.guesses, initArray, 9);
-}
-
-void set_value(solve_context &ctx, int row, int col, int value, int guess_depth, bool is_guess) {
-    bt_print(FormatNum("Set Value: %d", value));
-    bt_print(FormatNum(" (%d", row));
-    bt_print(FormatNum(", %d)\n", col));
-
-    ctx.board[row][col] = value;
-    ctx.guesses[row][col] = is_guess;
-    ctx.guess_depths[row][col] = guess_depth;
-    ctx.solved_count++;
-
-    int bI = boxIndex(row, col);
-
-    for (int i = 0; i < 9; i++) {
-        ctx.position_possibilities[row][i] &= ~numToBit(value);
-        ctx.position_possibilities[i][col] &= ~numToBit(value);
-        ctx.position_possibilities[getBoxRow(bI, i)][getBoxCol(bI, i)] &= ~numToBit(value);
-    }
-}
-
-void reset_guess(solve_context &ctx, int depth, int &guess_row, int &guess_col, int &guessed_value) {
-  for (int r = 0; r < 9; r++) {
-    for (int c = 0; c < 9; c++) {
-      if (ctx.guess_depths[r][c] == depth) {
-        int value = ctx.board[r][c];
-
-        if (ctx.guesses[r][c]) {
-          guess_row = r;
-          guess_col = c;
-          guessed_value = value;
-        }
-
-        ctx.solved_count--;
-        ctx.board[r][c] = 0;
-        ctx.guesses[r][c] = false;
-        ctx.guess_depths[r][c] = 0;
-
-        int bI = boxIndex(r, c);
-        for (int i = 0; i < 9; i++) {
-          ctx.position_possibilities[r][i] |= numToBit(value);
-          ctx.position_possibilities[i][c] |= numToBit(value);
-          ctx.position_possibilities[getBoxRow(bI, i)][getBoxCol(bI, i)] |= numToBit(value);
-        }
-      }
-    }
-  }
-}
-
-void init_solve_context(solve_context &ctx, int &board[][]) {
-    init_board(ctx);
-    init_possibilities(ctx);
-    init_depths(ctx);
-    init_guesses(ctx);
-    ctx.solved_count = 0;
-
-    for (int i = 0; i < 9; i++) {
-        for (int j = 0; j < 9; j++) {
-            if (board[i][j] != 0) {
-                set_value(ctx, i, j, board[i][j], 0, false);
-            }
-        }
-    }
-}
-
-void copy_solve_context(solve_context &src, solve_context &dest) {
-  init_board(dest);
-  init_possibilities(dest);
-  dest.solved_count = src.solved_count;
-
-  for (int i = 0; i < 9; i++) {
-    for (int j = 0; j < 9; j++) {
-      dest.board[i][j] = src.board[i][j];
-      dest.position_possibilities[i][j] = src.position_possibilities[i][j];
-    }
-  }
-}
-
 // Returns whether or not a board is a valid, solved sudoku
-bool is_valid_solution(solve_context &ctx) {
+bool is_valid_solution() {
   // Check if the sudoku is solved and valid
   int rows[9];
   int cols[9];
@@ -136,7 +143,7 @@ bool is_valid_solution(solve_context &ctx) {
 
   for (int r = 0; r < 9; r++) {
     for (int c = 0; c < 9; c++) {
-      int value = ctx.board[r][c];
+      int value = solve_board[r][c];
       if (value == 0) {
         // There's an empty space, so it isn't solved
         return false;
@@ -151,10 +158,7 @@ bool is_valid_solution(solve_context &ctx) {
   }
 
   for (int i = 0; i < 9; i++) {
-    // Since the smallest number in the grid is 1, the expected bit
-    // pattern for all numbers found would be this:
-    // 0011 1111 1110
-    int all_found = 0x3FE;
+    int all_found = ALL_POSSIBILITIES;
 
     if (rows[i] != all_found || cols[i] != all_found || boxes[i] != all_found) {
       return false;
